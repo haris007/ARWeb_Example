eb29c6964e5ae86e6a02b499ccf3a1224af1d90f
haris007 <haris.sidd786@gmail.com>
2021-01-16 21:08:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -53,6 +53,29 @@ window.onload = () => {
                     placeText.addEventListener('loaded', () => {
                         window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
                     });
+
+                    const clickListener = function (ev) {
+                        ev.stopPropagation();
+                        ev.preventDefault();
+            
+                        const name = ev.target.getAttribute('name');
+            
+                        const el = ev.detail.intersection && ev.detail.intersection.object.el;
+            
+                        if (el && el === ev.target) {
+                            const label = document.createElement('span');
+                            const container = document.createElement('div');
+                            container.setAttribute('id', 'place-label');
+                            label.innerText = name;
+                            container.appendChild(label);
+                            document.body.appendChild(container);
+            
+                            setTimeout(() => {
+                                container.parentElement.removeChild(container);
+                            }, 1500);
+                        }
+                    };
+                    placeText.addEventListener('click', clickListener);
                     scene.appendChild(placeText);
                 });
             })
